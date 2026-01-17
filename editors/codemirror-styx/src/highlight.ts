5d867789cd9192689e4943bbada1a785c399e82d
Amos Wenger <amos@bearcove.eu>
2026-01-17 21:11:57 +0100

    feat: add distinct colors for keys vs values
    Use path-based styling to differentiate:
    - Keys (property names) in blue/purple
    - Values (strings) in their respective colors
    - Sequence items styled as values

--- editors/codemirror-styx/src/highlight.ts
+++ editors/codemirror-styx/src/highlight.ts
@@ -1,11 +1,32 @@
 import { styleTags, tags as t } from "@lezer/highlight";
 
 export const styxHighlight = styleTags({
+  // Keys (property names) - first atom in an Entry
+  "KeyAtom/BareScalar": t.propertyName,
+  "KeyAtom/QuotedScalar": t.propertyName,
+  "KeyPayload/BareScalar": t.propertyName,
+  "KeyPayload/QuotedScalar": t.propertyName,
+
+  // Values - second atom in an Entry
+  "ValueAtom/BareScalar": t.string,
+  "ValueAtom/QuotedScalar": t.string,
+  "ValuePayload/BareScalar": t.string,
+  "ValuePayload/QuotedScalar": t.string,
+
+  // Sequence items
+  "SeqAtom/BareScalar": t.string,
+  "SeqAtom/QuotedScalar": t.string,
+  "SeqPayload/BareScalar": t.string,
+  "SeqPayload/QuotedScalar": t.string,
+
+  // Tags (@foo)
   Tag: t.tagName,
-  BareScalar: t.string,
-  QuotedScalar: t.string,
+
+  // Raw strings and heredocs
   RawScalar: t.special(t.string),
   Heredoc: t.special(t.string),
+
+  // Other
   Attributes: t.attributeName,
   Unit: t.null,
   Comment: t.lineComment,
